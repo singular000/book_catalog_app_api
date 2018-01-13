60c129667bd80f8e6dbf8ef5fb384e41ad796c32
Thom Page <thom.page.000@gmail.com>
2018-01-13 00:26:22 -0500

    Add require dotenv-rails to config to get it to work again

--- config/application.rb
+++ config/application.rb
@@ -30,5 +30,6 @@ module BookCatalogAppApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    require "dotenv-rails"
   end
 end
