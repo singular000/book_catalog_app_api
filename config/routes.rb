4bcb21af5e711dc9e97f9f1f591be39e327f6da5
Thom Page <thom.page.000@gmail.com>
2018-01-12 19:56:34 -0500

    add comma

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :users do
     collection do
-      post '/login' to: 'users#login'
+      post '/login', to: 'users#login'
     end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
