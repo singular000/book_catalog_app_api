966f69c5343d98901c6c43d95c9a61d76adf9c37
Thom Page <thom.page.000@gmail.com>
2018-01-12 23:11:10 -0500

    Develop authentication methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,20 @@ class ApplicationController < ActionController::API
 
   def authenticate_token
     puts "AUTHENTICATE JWT"
+    render json: { status: 401, message: 'Unauthorized' } unless decode_token(bearer_token)
+  end
+
+  def bearer_token
+    puts "BEARER TOKEN"
+    puts header = request.env['HTTP_AUTHORIZATION']
+
+    pattern = /^Bearer /
+    puts "TOKEN WITHOUT BEARER"
+    puts header.gsub(pattern, '') if header && header.match(pattern)
+  end
+
+  def decode_token(token_input)
+    puts "DECODE TOKEN: #{token_input}"
   end
 
 end
