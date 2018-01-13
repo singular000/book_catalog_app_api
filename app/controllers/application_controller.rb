d49771b8bc8311efc554fc47c0ef32c8c81e71fe
Thom Page <thom.page.000@gmail.com>
2018-01-12 23:30:10 -0500

    Add method to get current user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,4 +16,9 @@ class ApplicationController < ActionController::API
     render json: { status: 401, message: 'Unauthorized' }
   end
 
+  def get_current_user
+    return if !bearer_token
+    decoded_jwt = decode_token(bearer_token)
+    User.find(decoded_jwt[0]["user"]["id"]
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
 
   # GET /users/1
   def show
-    render json: @user
+    render json: get_current_user
   end
 
   # POST /users
